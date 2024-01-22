bd3fc3b38e3ebd0e01cd204d0e6a20653550e431
Altug Aslan <altgasln5734@gmail.com>
2024-01-22 22:46:07 +0300

    errorhandling1

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import image from "../../public/images/404.jpg"
+import Image from "../../public/images/404.jpg"
 const NotFound = () => {
     return (
         <div className="container">
             <div className="row align-items-center justify-content-center">
                 <div className="col-md-4">
-                <Image src={image} className="img-fluid" alt="Not found"/>
+                <Image src={Image} className="img-fluid" alt="Not found"/>
                 </div>
                 <div className="col-md-4 d-flex flex-column pt-4 text-center text-md-start">
                     <h2 className="text-primary fs-1">Not Found-Root</h2>
